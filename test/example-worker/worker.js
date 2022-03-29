97604ad881f18da77229ad6d2c3675fc1817ab1b
Dylan <dylanblokhuis3@hotmail.com>
2022-03-29 23:15:55 +0200

    remove useless folder

--- test/example-worker/worker.js
+++
@@ -1,9 +0,0 @@
-async function onRequest(event) {
-  const response = new Response("Hello from a worker!", {
-    headers: {
-      "X-Proto": "Test"
-    }
-  });
-
-  event.respondWith(response)
-}
